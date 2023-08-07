a0dd6d8b5a31bf08e129534d398725f2e8eed20b
Shukor, Halim <shukor.h@duerr-ndt.com>
2023-08-07 12:05:13 +0800

    sample dummy qt

--- main.py
+++ main.py
@@ -1,54 +1,55 @@
 import sys
-from PySide6.QtCore import Qt
 from PySide6.QtWidgets import (
     QApplication,
-    QHBoxLayout,
+    QStackedWidget,
     QLabel,
-    QMainWindow,
     QPushButton,
-    QStackedLayout,
     QVBoxLayout,
     QWidget,
 )
-from layout_colorwidget import Color
 
 
-class MainWindow(QMainWindow):
-    def __init__(self):
-        super().__init__()
-        self.setWindowTitle("My App")
-        pagelayout = QVBoxLayout()
-        button_layout = QHBoxLayout()
-        self.stacklayout = QStackedLayout()
-        pagelayout.addLayout(button_layout)
-        pagelayout.addLayout(self.stacklayout)
-        btn = QPushButton("red")
-        btn.pressed.connect(self.activate_tab_1)
-        button_layout.addWidget(btn)
-        self.stacklayout.addWidget(Color("red"))
-        btn = QPushButton("green")
-        btn.pressed.connect(self.activate_tab_2)
-        button_layout.addWidget(btn)
-        self.stacklayout.addWidget(Color("green"))
-        btn = QPushButton("yellow")
-        btn.pressed.connect(self.activate_tab_3)
-        button_layout.addWidget(btn)
-        self.stacklayout.addWidget(Color("yellow"))
-        widget = QWidget()
-        widget.setLayout(pagelayout)
-        self.setCentralWidget(widget)
-
-    def activate_tab_1(self):
-        self.stacklayout.setCurrentIndex(0)
-
-    def activate_tab_2(self):
-        self.stacklayout.setCurrentIndex(1)
-
-    def activate_tab_3(self):
-        self.stacklayout.setCurrentIndex(2)
+def create_page(text):
+    page = QWidget()
+    layout = QVBoxLayout()
+    label = QLabel(text)
+    layout.addWidget(label)
+    page.setLayout(layout)
+    return page
 
 
-app = QApplication(sys.argv)
-window = MainWindow()
-window.show()
-app.exec()
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    stacked_widget = QStackedWidget()
+
+    page1 = create_page("This is Page 1")
+    page2 = create_page("This is Page 2")
+    page3 = create_page("This is Page 3")
+
+    stacked_widget.addWidget(page1)
+    stacked_widget.addWidget(page2)
+    stacked_widget.addWidget(page3)
+
+    button_previous = QPushButton("Previous")
+    button_next = QPushButton("Next")
+
+    def go_to_previous_page():
+        stacked_widget.setCurrentIndex(stacked_widget.currentIndex() - 1)
+
+    def go_to_next_page():
+        stacked_widget.setCurrentIndex(stacked_widget.currentIndex() + 1)
+
+    button_previous.clicked.connect(go_to_previous_page)
+    button_next.clicked.connect(go_to_next_page)
+
+    layout = QVBoxLayout()
+    layout.addWidget(stacked_widget)
+    layout.addWidget(button_previous)
+    layout.addWidget(button_next)
+
+    main_widget = QWidget()
+    main_widget.setLayout(layout)
+    main_widget.setWindowTitle("QStackedWidget Example")
+    main_widget.show()
+
+    sys.exit(app.exec())
